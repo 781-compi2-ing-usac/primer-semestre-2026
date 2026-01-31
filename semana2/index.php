fed599c8bcec3a029b17509324fd57c8bd28611f
Diego Cali <diegocali123@gmail.com>
2026-01-31 15:59:52 -0600

    refactored week 2 with antlrv4

--- semana2/index.php
+++ semana2/index.php
@@ -1,52 +1,19 @@
-<!DOCTYPE html>
-<html>
-<title>Calculadora</title>
-<body>
 <?php
-    $input = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $input = $_POST["expression"];        
-    }
-?>
 
-    <h1>Calculadora Simple</h1>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-        <input type="text" name="expression" placeholder="Ingrese una expresión matemática">
-        <input type="submit" value="Calcular">
-    </form>
-<?php
-require_once("bootstrap.php");
+require __DIR__ . '/vendor/autoload.php';
+require_once 'bootstrap.php';
 
-$grammar = new calc();
-$parser = new parse_engine($grammar);
+use Antlr\Antlr4\Runtime\InputStream;
+use Antlr\Antlr4\Runtime\CommonTokenStream;
 
-echo "<h2>Resultado:</h2>";
-if (!empty($input)) {
-    try {
-        $parser->reset();
-        $lexer = new Lexer($input);
-        while (true) {
-            $token = $lexer->nextToken();
-            $tok = $token[0];
-            $val = $token[1];
+$input = InputStream::fromString('3 + 5 * (2 - 8)');
 
-            if ($tok == "EOF") {
-                $ast = $parser->eat_eof();
-                break;
-            }
-            $parser->eat($tok, $val);
-        }  
-        $interpreter = new Interpreter();
-        $result = $ast->accept($interpreter);   
-        echo "El resultado de la expresión '" . htmlspecialchars($input) . "' es: " . $result;       
-    }catch(Exception $e) {
-        echo "Error: " . $e->getMessage();
-    }
-} else {
-    echo "Por favor ingrese una expresión matemática.";
-}
-?>
-</body>
-</html>
+$lexer = new GrammarLexer($input);
+$tokens = new CommonTokenStream($lexer);
+$parser = new GrammarParser($tokens);
 
+$tree = $parser->p();
 
+$interpreter = new Interpreter();
+$result = $interpreter->visit($tree);
+echo "Result: " . $result . PHP_EOL;
